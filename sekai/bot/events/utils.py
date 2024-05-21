e40882bec554192853571bed9d7c500c3f718585
Ricky8955555 <rkmiao@duck.com>
2024-05-22 00:42:25 +0800

    fix(bot): incorrect string to represent False

--- sekai/bot/events/utils.py
+++ sekai/bot/events/utils.py
@@ -1,6 +1,6 @@
 def parse_bool(string: str) -> bool:
     if string in ["t", "true", "T", "True", "TRUE", "1", "yes", "Yes", "YES", "y", "Y"]:
         return True
-    if string in ["f", "false", "F", "False", "FALSE", "0", "no", "No", "NO", "y", "Y"]:
+    if string in ["f", "false", "F", "False", "FALSE", "0", "no", "No", "NO", "n", "N"]:
         return False
     raise ValueError
